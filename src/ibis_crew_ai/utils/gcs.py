49f73df94243525ee3cd6b1b92ad73dbe00de282
actuaristai <111639153+actuaristai@users.noreply.github.com>
2025-04-11 03:58:36 +0000

    refactor: pass lint

--- src/ibis_crew_ai/utils/gcs.py
+++ src/ibis_crew_ai/utils/gcs.py
@@ -1,21 +1,12 @@
-# Copyright 2025 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""Utility functions for Google Cloud Storage (GCS) operations."""
+
 
 import logging
 
 from google.api_core import exceptions
 from google.cloud import storage
+
+logger = logging.getLogger(__name__)
 
 
 def create_bucket_if_not_exists(bucket_name: str, project: str, location: str) -> None:
@@ -28,15 +19,14 @@ def create_bucket_if_not_exists(bucket_name: str, project: str, location: str) -
     """
     storage_client = storage.Client(project=project)
 
-    if bucket_name.startswith("gs://"):
-        bucket_name = bucket_name[5:]
+    bucket_name = bucket_name.removeprefix('gs://')
     try:
         storage_client.get_bucket(bucket_name)
-        logging.info(f"Bucket {bucket_name} already exists")
+        logger.info('Bucket already exists', extra={'bucket_name': bucket_name})
     except exceptions.NotFound:
-        bucket = storage_client.create_bucket(
-            bucket_name,
-            location=location,
-            project=project,
-        )
-        logging.info(f"Created bucket {bucket.name} in {bucket.location}")
+        bucket = storage_client.create_bucket(bucket_name,
+                                              location=location,
+                                              project=project)
+        logger.info('Created bucket in location',
+                    extra={'bucket_name': bucket.name,
+                           'bucket_location': bucket.location})
