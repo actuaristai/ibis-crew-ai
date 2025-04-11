96be7ac4bc983f29c027e1368e7d154921f529e9
actuaristai <111639153+actuaristai@users.noreply.github.com>
2025-04-11 04:59:43 +0000

    refactor: use loguru

--- src/ibis_crew_ai/utils/gcs.py
+++ src/ibis_crew_ai/utils/gcs.py
@@ -1,12 +1,27 @@
 """Utility functions for Google Cloud Storage (GCS) operations."""
 
-
-import logging
-
 from google.api_core import exceptions
+from google.cloud import logging as google_cloud_logging
 from google.cloud import storage
+from loguru import logger
 
-logger = logging.getLogger(__name__)
+# Initialize Google Cloud Logging
+logging_client = google_cloud_logging.Client()
+gcloud_logger = logging_client.logger(__name__)
+
+# Add a custom sink to forward loguru logs to Google Cloud Logging
+
+
+class GoogleCloudSink:
+    """Custom sink to forward loguru logs to Google Cloud Logging."""
+
+    def write(self, message: str) -> None:
+        """Write a log message to Google Cloud Logging."""
+        record = message.strip()
+        gcloud_logger.log_text(record)
+
+
+logger.add(GoogleCloudSink(), level='INFO')  # Forward loguru logs to Google Cloud Logging
 
 
 def create_bucket_if_not_exists(bucket_name: str, project: str, location: str) -> None:
@@ -22,11 +37,11 @@ def create_bucket_if_not_exists(bucket_name: str, project: str, location: str) -
     bucket_name = bucket_name.removeprefix('gs://')
     try:
         storage_client.get_bucket(bucket_name)
-        logger.info('Bucket already exists', extra={'bucket_name': bucket_name})
+        logger.info('Bucket already exists', bucket_name=bucket_name)
     except exceptions.NotFound:
         bucket = storage_client.create_bucket(bucket_name,
                                               location=location,
                                               project=project)
         logger.info('Created bucket in location',
-                    extra={'bucket_name': bucket.name,
-                           'bucket_location': bucket.location})
+                    bucket_name=bucket.name,
+                    bucket_location=bucket.location)
